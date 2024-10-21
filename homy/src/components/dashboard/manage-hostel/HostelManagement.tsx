4b1ba5976fc549af6af75ff9c1ebad462833a30f
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-10-21 15:36:16 +0700

    feature : pagaing list hostel

--- homy/src/components/dashboard/manage-hostel/HostelManagement.tsx
+++ homy/src/components/dashboard/manage-hostel/HostelManagement.tsx
@@ -6,18 +6,19 @@ import Link from "next/link";
 import Image from "next/image";
 import AddHostelForm from "../add-hostel/AddHostelForm"; // Import your AddHostelForm component
 import icon_1 from "@/assets/images/icon/icon_46.svg";
+import useHostels from "./useHostels";
 
 const HostelManagement = () => {
    const selectHandler = (e: any) => { };
-
+   const {hostels,totalPages, pageIndex, setPageIndex, totalRecords, loading} = useHostels();
    return (
       <div className="dashboard-body">
          <div className="position-relative">
             <DashboardHeaderTwo title="My Properties" />
             <h2 className="main-title d-block d-lg-none">My Properties</h2>
             <div className="d-sm-flex align-items-center justify-content-between mb-25">
-               <div className="fs-16">Showing <span className="color-dark fw-500">1–5</span> of <span
-                  className="color-dark fw-500">40</span> results</div>
+               <div className="fs-16">Hiển thị <span className="color-dark fw-500">{(pageIndex - 1) * 10 + 1}</span>  to{" "} <span
+                  className="color-dark fw-500">{totalRecords}</span> kết quả</div>
                <div className="d-flex ms-auto xs-mt-30">
                   <div className="short-filter d-flex align-items-center ms-sm-auto">
                      <div className="fs-16 me-2">Sort by:</div>
@@ -48,26 +49,29 @@ const HostelManagement = () => {
                   <table className="table property-list-table">
                      <thead>
                         <tr>
-                           <th scope="col">Title</th>
-                           <th scope="col">Date</th>
-                           <th scope="col">View</th>
-                           <th scope="col">Status</th>
-                           <th scope="col">Action</th>
+                           <th scope="col">Phòng trọ</th>
+                           <th scope="col">Diện tích</th>
+                           <th scope="col">Đánh giá</th>
+                           <th scope="col">Ảnh</th>
+                           <th scope="col">Hành động</th>
                         </tr>
                      </thead>
-                     <PropertyTableBody />
+                     <PropertyTableBody hostels={hostels} loading ={loading} />
                   </table>
                </div>
             </div>
 
             <ul className="pagination-one d-flex align-items-center justify-content-center style-none pt-40">
-               <li className="me-3"><Link href="#">1</Link></li>
-               <li className="selected"><Link href="#">2</Link></li>
-               <li><Link href="#">3</Link></li>
-               <li><Link href="#">4</Link></li>
-               <li>....</li>
-               <li className="ms-2"><Link href="#" className="d-flex align-items-center">
-                  Last <Image src={icon_1} alt="" className="ms-2" /></Link></li>
+               {[...Array(totalPages)].map((_, index) => (
+                  <li key={index} className={pageIndex === index + 1 ? "selected" : ""}>
+                     <button onClick={() => setPageIndex(index + 1)}>{index + 1}</button>
+                  </li>
+               ))}
+               <li className="ms-2">
+                  <Link href="#" className="d-flex align-items-center">
+                     Last <Image src={icon_1} alt="" className="ms-2" />
+                  </Link>
+               </li>
             </ul>
          </div>
       </div>
