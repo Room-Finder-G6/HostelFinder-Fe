797add2bbfac6dff45169a6e118f080c5dc587ac
vietthaist01@gmail.com <vietthaist01@gmail.com>
2024-10-25 16:05:50 +0700

    done

--- homy/src/layouts/headers/dashboard/DashboardHeaderOne.tsx
+++ homy/src/layouts/headers/dashboard/DashboardHeaderOne.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image"
 import Link from "next/link";
-import { usePathname } from 'next/navigation'
+import {usePathname} from 'next/navigation'
 
 import dashboardLogo from "@/assets/images/logo/logo_01.svg";
 import dashboardIconActive_1 from "@/assets/images/dashboard/icon/icon_1_active.svg";
@@ -27,88 +27,125 @@ import dashboardIcon_10 from "@/assets/images/dashboard/icon/icon_10.svg";
 import dashboardIcon_11 from "@/assets/images/dashboard/icon/icon_31.svg";
 
 
+const DashboardHeaderOne = ({isActive, setIsActive}: any) => {
+    const pathname = usePathname();
 
-const DashboardHeaderOne = ({ isActive, setIsActive }: any) => {
-   const pathname = usePathname();
+    return (
+        <aside className={`dash-aside-navbar ${isActive ? "show" : ""}`}>
+            <div className="position-relative">
+                <div
+                    className="logo d-md-block d-flex align-items-center justify-content-between plr bottom-line pb-30">
+                    <Link href="/">
+                        <div className="logo-container" style={{display: 'flex', alignItems: 'center'}}>
+                            <Image src="/assets/images/logo/logo_06.svg" alt=" PhongTro24/7" width={50} height={50}/>
+                            <span className="logo-text" style={{fontSize: '25px', color: 'black', marginRight: '10px'}}>
+                                <strong>&nbsp;PhongTro24/7</strong>
+                            </span>
+                        </div>
+                    </Link>
+                    <button onClick={() => setIsActive(false)} className="close-btn d-block d-md-none"><i
+                        className="fa-light fa-circle-xmark"></i></button>
+                </div>
+                <nav className="dasboard-main-nav pt-30 pb-30 bottom-line">
+                    <ul className="style-none">
+                        <li className="plr"><Link href="/dashboard/dashboard-index"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/dashboard-index' ? 'active' : ''}`}>
+                            <Image
+                                src={pathname === '/dashboard/dashboard-index' ? dashboardIconActive_1 : dashboardIcon_1}
+                                alt=""/>
+                            <span>Dashboard</span>
+                        </Link></li>
+                        <li className="plr"><Link href="/dashboard/message"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/message' ? 'active' : ''}`}>
+                            <Image src={pathname === '/dashboard/message' ? dashboardIconActive_2 : dashboardIcon_2}
+                                   alt=""/>
+                            <span>Message</span>
+                        </Link></li>
+                        <li className="bottom-line pt-30 lg-pt-20 mb-40 lg-mb-30"></li>
+                        <li>
+                            <div className="nav-title">Profile</div>
+                        </li>
+                        <li className="plr"><Link href="/dashboard/profile"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/profile' ? 'active' : ''}`}>
+                            <Image src={pathname === '/dashboard/profile' ? dashboardIconActive_3 : dashboardIcon_3}
+                                   alt=""/>
+                            <span>Profile</span>
+                        </Link></li>
+                        <li className="plr"><Link href="/dashboard/account-settings"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/account-settings' ? 'active' : ''}`}>
+                            <Image
+                                src={pathname === '/dashboard/account-settings' ? dashboardIconActive_4 : dashboardIcon_4}
+                                alt=""/>
+                            <span>Account Settings</span>
+                        </Link></li>
+                        <li className="plr"><Link href="/dashboard/membership"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/membership' ? 'active' : ''}`}>
+                            <Image src={pathname === '/dashboard/membership' ? dashboardIconActive_5 : dashboardIcon_5}
+                                   alt=""/>
+                            <span>Membership</span>
+                        </Link></li>
+                        <li className="bottom-line pt-30 lg-pt-20 mb-40 lg-mb-30"></li>
+                        <li>
+                            <div className="nav-title">Listing</div>
+                        </li>
+                        <li className="plr"><Link href="/dashboard/properties-list"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/properties-list' ? 'active' : ''}`}>
+                            <Image
+                                src={pathname === '/dashboard/properties-list' ? dashboardIconActive_6 : dashboardIcon_6}
+                                alt=""/>
+                            <span>List Room</span>
+                        </Link></li>
+                        <li className="plr"><Link href="/dashboard/manage-hostels"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/manage-hostels' ? 'active' : ''}`}>
+                            <Image src={pathname === '/dashboard/add-manager' ? dashboardIconActive_7 : dashboardIcon_7}
+                                   alt=""/>
+                            <span>Quản Lý Nhà Trọ</span>
+                        </Link></li>
+                        <li className="plr"><Link href="/dashboard/manage-post"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/manage-post' ? 'active' : ''}`}>
+                            <Image src={pathname === '/dashboard/manage-post' ? dashboardIconActive_7 : dashboardIcon_7}
+                                   alt=""/>
+                            <span>Quản Lý Bài Đăng</span>
+                        </Link></li>
+                        <li className="plr"><Link href="/dashboard/favourites"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/favourites' ? 'active' : ''}`}>
+                            <Image src={pathname === '/dashboard/favourites' ? dashboardIconActive_8 : dashboardIcon_8}
+                                   alt=""/>
+                            <span>Favourites</span>
+                        </Link></li>
+                        <li className="plr"><Link href="/dashboard/saved-search"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/saved-search' ? 'active' : ''}`}>
+                            <Image
+                                src={pathname === '/dashboard/saved-search' ? dashboardIconActive_9 : dashboardIcon_9}
+                                alt=""/>
+                            <span>Saved Search</span>
+                        </Link></li>
+                        <li className="plr"><Link href="/dashboard/review"
+                                                  className={`d-flex w-100 align-items-center ${pathname === '/dashboard/review' ? 'active' : ''}`}>
+                            <Image src={pathname === '/dashboard/review' ? dashboardIconActive_10 : dashboardIcon_10}
+                                   alt=""/>
+                            <span>Reviews</span>
+                        </Link></li>
+                    </ul>
+                </nav>
+                <div className="profile-complete-status bottom-line pb-35 plr">
+                    <div className="progress-value fw-500">82%</div>
+                    <div className="progress-line position-relative">
+                        <div className="inner-line" style={{width: "80%"}}></div>
+                    </div>
+                    <p>Profile Complete</p>
+                </div>
 
-   return (
-      <aside className={`dash-aside-navbar ${isActive ? "show" : ""}`}>
-         <div className="position-relative">
-            <div className="logo d-md-block d-flex align-items-center justify-content-between plr bottom-line pb-30">
-               <Link href="/dashboard-index">
-                  <Image src={dashboardLogo} alt="" />
-               </Link>
-               <button onClick={() => setIsActive(false)} className="close-btn d-block d-md-none"><i className="fa-light fa-circle-xmark"></i></button>
+                <div className="plr">
+                    <Link href="#" className="d-flex w-100 align-items-center logout-btn">
+                        <div className="icon tran3s d-flex align-items-center justify-content-center rounded-circle">
+                            <Image src={dashboardIcon_11} alt=""/></div>
+                        <span>Logout</span>
+                    </Link>
+                </div>
             </div>
-            <nav className="dasboard-main-nav pt-30 pb-30 bottom-line">
-               <ul className="style-none">
-                  <li className="plr"><Link href="/dashboard/dashboard-index" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/dashboard-index' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/dashboard-index' ? dashboardIconActive_1 : dashboardIcon_1} alt="" />
-                     <span>Dashboard</span>
-                  </Link></li>
-                  <li className="plr"><Link href="/dashboard/message" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/message' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/message' ? dashboardIconActive_2 : dashboardIcon_2} alt="" />
-                     <span>Message</span>
-                  </Link></li>
-                  <li className="bottom-line pt-30 lg-pt-20 mb-40 lg-mb-30"></li>
-                  <li><div className="nav-title">Profile</div></li>
-                  <li className="plr"><Link href="/dashboard/profile" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/profile' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/profile' ? dashboardIconActive_3 : dashboardIcon_3} alt="" />
-                     <span>Profile</span>
-                  </Link></li>
-                  <li className="plr"><Link href="/dashboard/account-settings" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/account-settings' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/account-settings' ? dashboardIconActive_4 : dashboardIcon_4} alt="" />
-                     <span>Account Settings</span>
-                  </Link></li>
-                  <li className="plr"><Link href="/dashboard/membership" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/membership' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/membership' ? dashboardIconActive_5 : dashboardIcon_5} alt="" />
-                     <span>Membership</span>
-                  </Link></li>
-                  <li className="bottom-line pt-30 lg-pt-20 mb-40 lg-mb-30"></li>
-                  <li><div className="nav-title">Listing</div></li>
-                  <li className="plr"><Link href="/dashboard/properties-list" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/properties-list' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/properties-list' ? dashboardIconActive_6 : dashboardIcon_6} alt="" />
-                     <span>List Room</span>
-                  </Link></li>
-                  <li className="plr"><Link href="/dashboard/manage-hostels" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/manage-hostels' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/add-manager' ? dashboardIconActive_7 : dashboardIcon_7} alt="" />
-                     <span>Quản Lý Nhà Trọ</span>
-                  </Link></li>
-                  <li className="plr"><Link href="/dashboard/manage-post" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/manage-post' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/manage-post' ? dashboardIconActive_7 : dashboardIcon_7} alt="" />
-                     <span>Quản Lý Bài Đăng</span>
-                  </Link></li>
-                  <li className="plr"><Link href="/dashboard/favourites" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/favourites' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/favourites' ? dashboardIconActive_8 : dashboardIcon_8} alt="" />
-                     <span>Favourites</span>
-                  </Link></li>
-                  <li className="plr"><Link href="/dashboard/saved-search" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/saved-search' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/saved-search' ? dashboardIconActive_9 : dashboardIcon_9} alt="" />
-                     <span>Saved Search</span>
-                  </Link></li>
-                  <li className="plr"><Link href="/dashboard/review" className={`d-flex w-100 align-items-center ${pathname === '/dashboard/review' ? 'active' : ''}`}>
-                     <Image src={pathname === '/dashboard/review' ? dashboardIconActive_10 : dashboardIcon_10} alt="" />
-                     <span>Reviews</span>
-                  </Link></li>
-               </ul>
-            </nav>
-            <div className="profile-complete-status bottom-line pb-35 plr">
-               <div className="progress-value fw-500">82%</div>
-               <div className="progress-line position-relative">
-                  <div className="inner-line" style={{ width: "80%" }}></div>
-               </div>
-               <p>Profile Complete</p>
-            </div>
-
-            <div className="plr">
-               <Link href="#" className="d-flex w-100 align-items-center logout-btn">
-                  <div className="icon tran3s d-flex align-items-center justify-content-center rounded-circle"><Image src={dashboardIcon_11} alt="" /></div>
-                  <span>Logout</span>
-               </Link>
-            </div>
-         </div>
-      </aside>
-   )
+        </aside>
+    )
 }
 
 export default DashboardHeaderOne;
