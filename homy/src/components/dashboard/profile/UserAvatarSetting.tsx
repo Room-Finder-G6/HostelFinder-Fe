4e4a4aa4e252ff4733da8afcbfa7a01d0a65a9e8
vietthaist01@gmail.com <vietthaist01@gmail.com>
2024-11-17 18:05:52 +0700

    done update profile

--- homy/src/components/dashboard/profile/UserAvatarSetting.tsx
+++ homy/src/components/dashboard/profile/UserAvatarSetting.tsx
@@ -1,29 +1,43 @@
 import Link from "next/link"
+
 interface UserAvatarSettingProps {
     username: string;
     email: string;
     phone: string;
-
+    fullName: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 }
 
-const UserAvatarSetting: React.FC<UserAvatarSettingProps> = ({ username, email, phone, onChange }) => {
-    console.log("UserAvatarSetting props:", { username, email, phone }); // Log để kiểm tra dữ liệu truyền vào
+const UserAvatarSetting: React.FC<UserAvatarSettingProps> = ({fullName ,username, email, phone, onChange}) => {
 
     return (
         <div className="row">
             <div className="col-sm-6">
                 <div className="dash-input-wrapper mb-30">
-                    <label htmlFor="username">Username*</label>
+                    <label htmlFor="username">Tên*</label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={fullName}
+                        onChange={onChange}
+                        placeholder="Nhập tên của bạn"
+                    />
+                </div>
+            </div>
+
+            <div className="col-sm-6">
+                <div className="dash-input-wrapper mb-30">
+                    <label htmlFor="username">Tên đăng nhập*</label>
                     <input
                         type="text"
                         name="username"
                         value={username}
                         onChange={onChange}
-                        placeholder="Enter your username"
+                        placeholder="Nhập tên đăng nhập"
                     />
                 </div>
             </div>
+
             <div className="col-sm-6">
                 <div className="dash-input-wrapper mb-30">
                     <label htmlFor="email">Email*</label>
@@ -32,29 +46,26 @@ const UserAvatarSetting: React.FC<UserAvatarSettingProps> = ({ username, email, 
                         name="email"
                         value={email}
                         onChange={onChange}
-                        placeholder="Enter your email"
+                        placeholder="Nhập email"
                     />
                 </div>
             </div>
             <div className="col-sm-6">
                 <div className="dash-input-wrapper mb-30">
-                    <label htmlFor="phone">Phone Number*</label>
+                    <label htmlFor="phone">Số điện thoại*</label>
                     <input
                         type="tel"
                         name="phone"
                         value={phone}
                         onChange={onChange}
-                        placeholder="Enter your phone number"
+                        placeholder="Nhập số điện thoại"
                     />
                 </div>
                 <div className="info-text d-sm-flex align-items-center justify-content-between mt-5">
-                    <p className="m0">Want to change the password?
+                    <p className="m0">Đổi mật khẩu?
                         <Link href="/dashboard/account-settings/password-change">Click here</Link></p>
-                    
                 </div>
-
             </div>
-
         </div>
     );
 };
