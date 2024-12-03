0f468ec6fee58cf69a69cab4664a30263256f270
SyxHieu <133748573+SyxHieu@users.noreply.github.com>
2024-12-03 21:51:29 +0700

    ManageService

--- homy/src/components/dashboard/manage-service-landlord/ServiceTable.tsx
+++ homy/src/components/dashboard/manage-service-landlord/ServiceTable.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import PropertyTableBody from "../manage-hostel/PropertyTableBody";
 import HostelSelector from "./HostelSelector";  // Import HostelSelector component
 
-const RoomTable: React.FC = () => {
+const ServiceTable: React.FC = () => {
   return (
     <div className="bg-white card-box p0 border-20">
       <div className="table-responsive pt-25 pb-25 pe-4 ps-4">
         <table className="table property-list-table">
           <thead>
             <tr>
-              <th scope="col">Nhà trọ</th>
-              <th scope="col">Ngày tạo</th>
-              <th scope="col">Trạng thái</th>
-              <th scope="col"></th>
+              <th scope="col">Tên dịch vụ</th>
+              <th scope="col">Giá dịch vụ</th>
+              <th scope="col">Đơn vị tính</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <PropertyTableBody />
@@ -22,4 +22,4 @@ const RoomTable: React.FC = () => {
   );
 };
 
-export default RoomTable;
+export default ServiceTable;
