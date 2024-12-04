fcb3f06fd37754035ea4dadfd42381000597db93
Mai Trong Hieu <90890367+hieumt24@users.noreply.github.com>
2024-12-04 15:08:07 +0700

    Hieumt/develop (#83)
    * feature : redirect user, admin
    
    * refactor : login form
    
    * feature : add hostel fe
    
    * refactor : add hostel in fe
    
    * feature: add icon
    
    * refactor: update message register and button sign up
    
    * Fix registration form and dashboard header
    
    * refactor: fix call api add hostel
    
    * feature : login by google
    
    * refactor : add hostel
    
    * feature : pagaing list hostel
    
    * update auth provider
    
    * refactor : fix bug build ci/cd
    
    * refactor : merge main
    
    * feature : add post
    
    * feature : login and logout
    
    * bug : fix bug select location in add hostel
    
    * add image service
    
    * feature add service to hostel
    
    * feature : add room and add service hostel
    
    * feature : add room
    
    * feature(manage-room): upload room
    
    * add svg image
    
    * refactor code(manage room) : fix bug manager room
    
    * feature : add rentral contract
    
    * fix bug: fix pipeline
    
    * feature : manage room update interface
    
    * refactore code : remove taidwind and update boostrap in feature room
    
    * fix-bug : fix user login close popup
    
    * feature : update serivce and create invoice
    
    * feature(invoice): create invoice and write meter reading
    
    * refactor code : update validation invoice
    
    * feature(invoice) : report invoice
    
    * feauture report and invoice details
    
    * refactor code to main
    
    * refactor code
    
    * update redirect landlord
    
    * feature : collect invoice
    
    * feature(contract-rentral) : update filter
    
    * update write invoice
    
    * fix-bug: add meter Reading
    
    * refactor : update profile and meter reading
    
    * fix-bug : room Manage
    
    * update link
    
    * feature(update-room)
    
    * feature(login-with-google)
    
    * fix-bug : logout
    
    * feature(post) : AI generate post

--- homy/src/layouts/headers/dashboard/Profile.tsx
+++ homy/src/layouts/headers/dashboard/Profile.tsx
@@ -4,14 +4,18 @@ import { useRouter } from "next/navigation"; // Sử dụng useRouter trong Next
 import profileIcon_1 from "@/assets/images/dashboard/icon/icon_23.svg";
 import profileIcon_2 from "@/assets/images/dashboard/icon/icon_24.svg";
 import profileIcon_3 from "@/assets/images/dashboard/icon/icon_25.svg";
+import { signOut } from "next-auth/react";
 
 const Profile: React.FC = () => {
    const router = useRouter();
 
    const handleLogout = () => {
       localStorage.removeItem("token");
+      localStorage.removeItem("userName");
       router.refresh();
-      window.location.href = '/';
+      signOut({
+         callbackUrl: "/",
+      });
    };
 
    return (
@@ -28,7 +32,7 @@ const Profile: React.FC = () => {
                   <button
                      type="button"
                      className="dropdown-item d-flex align-items-center"
-                     onClick={handleLogout} 
+                     onClick={handleLogout}
                   >
                      <Image src={profileIcon_3} alt="Logout Icon" className="lazy-img" />
                      <span className="ms-2 ps-1">Đăng xuất</span>
