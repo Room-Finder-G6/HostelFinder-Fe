74648adbfc788fb42bf5920d734b967093108eb1
thainv <thainv@newwave.vn>
2024-12-11 22:43:03 +0700

    a

--- homy/src/layouts/headers/admin/AdminHeader.tsx
+++ homy/src/layouts/headers/admin/AdminHeader.tsx
@@ -96,7 +96,7 @@ const AdminHeader: React.FC<AdminHeaderProps> = ({ isActive, setIsActive }) => {
                     label: "Thống Kê Gói Người Dùng"
                 },
                 {
-                    href: "https://my.payos.vn/a1cd337babcb11ef964c0242ac110002/payment-link",
+                    href: "https://my.payos.vn/a1cd337babcb11ef964c0242ac110002/dashboard",
                     activeIcon: dashboardIconActive_4,
                     inactiveIcon: dashboardIcon_4,
                     label: "Thống Kê Tiền Nạp Hệ Thống"
